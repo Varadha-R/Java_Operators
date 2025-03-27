fa3cb3a12dfcd4d3c287c3cf264a987e2bd3b34e
Varadha Rajan <varadharaja695@gmail.com>
2025-03-27 13:18:04 +0530

    Add files via upload

---
+++ Day2.java
@@ -0,0 +1,212 @@
+public class Day2 {
+    public static void main(String[] args) {
+      
+    //Operators
+
+    //Arithmetic Opeartor (+,-,*,/,%,)
+
+   // int a = 20;
+   // int b = 10;
+
+  //  System.out.println(a+b);
+   // System.out.println(a-b);
+   // System.out.println(a*b);
+   // System.out.println(a/b);
+   // System.out.println(a%b);
+    
+//Assignment Opeartor(+=,-=,*=,/=)
+     
+       // int a = 20;
+       // int b = 10;
+       // a += b;
+      // a -= b;
+     // a *= b;
+      //a /= b;
+     //  System.out.println(a);
+
+     // Comparision Operator (>,<,>=,<=,==,!=)
+
+   //  int a = 20;
+   //  int b = 10;
+    // System.out.println(a>b);
+    // System.out.println(a<b);
+     //System.out.println(a>=b);
+     //System.out.println(a<=b);
+     //System.out.println(a==b);
+     //System.out.println(a!=b);
+
+     // Logical Operator (&& , || , !=)
+
+//int a = 20;
+//int b = 10;
+//boolean c = (a>10 && b>15);
+//System.out.println(c);
+//System.out.println(a> 10 || b>5);
+
+// Another Example for Logical Operator
+
+        //int a=5;
+       // int b=10;
+   // boolean check=(a<b && a<=b);
+   //boolean check = (a==b || a!=b);
+  // boolean check = ! (a!=b);
+   // System.out.println(check);
+
+
+// Increment Decrement Operator
+
+// Pre Increment and Post Increment
+
+// int x = 5;
+// System.out.println("Pre-Increment :"+(++x)); // 1 + 5
+// System.out.println("Post-Increment :"+(x++));// 5 + 1
+
+// Pre Decrement Post Decrement
+
+// int x = 4;
+// System.out.println("Pre-Decrement : "+(--x)); // 1-4
+// System.out.println("Post- Decrement : "+(x--)); // 4-1
+
+//Task For Operator 
+  
+// Before Swapping And After Swapping
+
+//int a = 10;
+//int b = 20;
+//a=a+b; // a=30
+//b=a-b; // b= 10
+//a=a-b; // a 20
+
+//System.out.println("a="+a);
+//System.out.println("b="+b);
+
+//int a = 10;
+//int b = 20;
+//int c = a;
+//a=b; //20
+//b=c; //10
+//System.out.println("a="+a);
+//System.out.println("b="+b);
+
+//Adding of 3 Number
+
+//int tamil= 100;
+//int english = 98;
+//int social = 99;
+ //int total = tamil+english+social;
+ //int average = total/3;
+//System.out.println(total);
+//System.out.println("average="+average );
+
+//Area of Circle
+
+//int radius = 10;
+//double area = Math.PI *radius *radius;
+//System.out.println("Area of the circle is : "+ area );
+
+//Area of rectangle
+
+//int Length = 10;
+//int Breadth = 20;
+ //int Area = Length*Breadth;
+//System.out.println("Area of Rectangle is :"+Area);
+
+//Area of Triangle
+
+//double base = 10;
+//double height = 5;
+//double area =  base * height / 2;
+//System.out.println("Area of Triangle :" + area);
+
+//Area of Square
+
+//int square = 6 ;
+//int area = square * square;
+//System.out.println("Area of Square : " + area);
+
+//Area of Rhombus
+
+//double PDiagonal = 5 ;
+//double QDiagonal = 10;
+//double area = PDiagonal*QDiagonal / 2;
+//System.out.println("Area of Rhombus :" + area);
+
+//Volume of Sphere
+
+//double radius = 10 ;
+//double volume = 4.0/3 * Math.PI * Math.pow(radius,3);
+//System.out.println("Volume of sphere:" + volume);
+
+//Volume of Hemisphere
+
+//double radius = 3 ;
+//double volume = 2.0/3 * Math.PI * Math.pow(radius, 3);
+//System.out.println("Volume of Hemisphere : " + volume);
+
+//Volume of Cube
+
+//double cube = 5;
+//double volume = Math.pow(cube,3);
+//System.out.println("Volume of Cube :" + volume);
+
+//Volume of Cuboid
+
+//double Length = 5;
+//double Width = 2;
+//double Height = 3;
+//double Cuboid = Length*Width*Height;
+//System.out.println("Volume of Cuboid:"+Cuboid);
+
+// Celsious to farenheit
+
+//double Celsious = 30;
+//double farenheit =(Celsious*9/5)+32;
+//System.out.println("farenheit is :"+ farenheit);
+
+//Farenheit to celsious
+
+//double farenheit = 39.4;
+//double celsious = (farenheit-32)*5/9;
+//System.out.println("Celcious :" + celsious);
+
+//Simple Intrest 
+
+//double Principal = 500000;
+//double Rate = 9.5;
+//double Time = 2;
+//double Calculate = Principal*Rate*Time / 100 ;
+//System.out.println("Simple Intrest : "+ Calculate);
+
+//Compound Intrest 
+
+//double p = 100000;
+//double r = 12;
+//double n = 2;
+//double calculate = p*Math.pow((1+r/100),n);
+//System.out.println("Compound Intrest : " + calculate);
+
+//Gst Calculator
+
+//int Amount = 1000;
+//int Calculate = Amount * 18/100;
+//System.out.println("Gst Calculation RS : "+ Calculate);
+
+//PPF Calculator
+
+//double p = 100000;
+//double r = 9.5/100;
+//double n = 15;
+//double Calculate = p*(Math.pow(1+r,n))-1 / r ;
+//System.out.println("PPF Calculator : " + Calculate);
+
+//Fd Calculator
+
+//double p = 100000;
+//double r = 8.0;
+//double t = 2;
+//double Calculate = (p*r*t)/100;
+//System.out.println("Fd Intrest for 2 Years :" + Calculate);
+
+    }
+}
+
